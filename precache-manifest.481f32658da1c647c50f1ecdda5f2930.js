4af438dbb772a4cc5f53fc3b678c5d7398aacc2e
Adam Morse <Morse1@missouristate.edu>
2020-02-14 12:44:05 -0600

    Updates

---
+++ precache-manifest.481f32658da1c647c50f1ecdda5f2930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8431023e5f92e17f629f2c7ae32c08e",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "dca0591094336fb86364",
+    "url": "/news/static/css/main.e31e654a.chunk.css"
+  },
+  {
+    "revision": "39586c2343b6e41af542",
+    "url": "/news/static/js/2.9275ef24.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/news/static/js/2.9275ef24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dca0591094336fb86364",
+    "url": "/news/static/js/main.8e21c6bc.chunk.js"
+  },
+  {
+    "revision": "6124cce38002b34abf0e",
+    "url": "/news/static/js/runtime-main.ea9d05ff.js"
+  }
+]);
